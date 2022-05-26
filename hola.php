de905ccafa6d6dff104a65e486c79207613012dd
Leily <leilymrz@gmail.com>
2022-05-25 22:42:06 -0700

    Learning php

---
+++ hola.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $_GET['title']; ?></title>
+</head>
+<body>
+
+  <h1>1st index :3</h1>
+  <!-- <div><?php echo $_GET['content']; ?></div> -->
+  <div><?php echo 'PHP version: ' . phpversion(); ?></div>
+  <div><?php echo phpinfo(); ?></div>
+  
+
+</body>
+</html>
