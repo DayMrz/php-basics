5b7c48f3e205180b9a37a864300ff74bbaa8ba52
Leily <leilymrz@gmail.com>
2022-05-26 16:46:55 -0700

    String functions

--- strings.php
+++ strings.php
@@ -8,9 +8,14 @@
 </head>
 <body>
   <h1>The God of High School</h1>
+  <h2>Jin Mori</h2>
+  <p>
+    <?php 
+      echo 'Jin Mori (진모리, Jin Mori; "Mori Jin") is the main protagonist of The God of High School. He is introduced as an arrogant, carefree child obsessed with fighting. <br>';
+      echo 'However his true identity is that of the great God Sun Wukong (The Monkey King), the leader of the Mount Hwagwa Monkey and one of the Nine Kings of the Sage Realm.';
+    ?>
+  </p>
   <?php 
-    echo 'Jin Mori (진모리, Jin Mori; "Mori Jin") is the main protagonist of The God of High School. He is introduced as an arrogant, carefree child obsessed with fighting. <br>';
-    echo 'However his true identity is that of the great God Sun Wukong (The Monkey King), the leader of the Mount Hwagwa Monkey and one of the Nine Kings of the Sage Realm.';
     $age = 17;
     $raceSpecies = ['God', 'Demon', 'Mount Hwagwa Monkey'];
     $occupation = ['G.O.H. Participant', 'Student', 'Monkey King', 'Supreme God'];
@@ -45,6 +50,67 @@
         echo 'his age is $age <br>';  //single quote with a nested variable prints out an string 
         echo "his age is $age <br>"; //double quote with a nested variable prints out a string with the proper value of the variable
       ?>
+    </li>
+  </ul>
+  <hr>
+  <p>*Concatenation examples*</p>
+  <h2>Yu Mira</h2>
+  <?php 
+    $paragraph1 = 'Yu Mira (유미라, Yu Mira; "Mira Yu"Mira yoo) is one of the three protagonists of The God of High School, and is the 25th Master of the Moon Light Sword Style.';
+    $paragraph2 = 'She was invited to participate in the God Of High School Tournament. During the World Tournament and RagnarÖk, she received Original Greed and through it Ultio R bequeath her Okhwangs physical power (which he had previously stolen)';
+    $combine = $paragraph1 ;
+    $combine .= $paragraph2;
+    ?>
+  <p>
+    <?php echo $combine?>
+  </p> 
+    <br>
+  <h3>Appearance</h3>
+  <span>*string functions examples*</span>
+    <?php 
+    $paragraph1 = 'initially, Mira had reddish brown, shoulder-length hair and generally wore round spectacles.';
+    $paragraph2 = 'she was usually seen wearing a full-sleeved tee along with a mini skirt during and outside of battles.';
+    $combine = $paragraph1 ;
+    $combine .= $paragraph2;
+    ?>
+  
+  <p>
+    Uppercase first: <br><?php echo ucfirst($combine); ?>
+  </p>
+  <p>
+    Uppercase words: <br> <?php echo ucwords($combine); ?>
+  </p>
+  <p>
+    Lowercase: <br> <?php echo strtolower($combine); ?>
+  </p>
+  <p>
+    Lowercase to uppercase: <br> 
+    <?php echo strtoupper($combine); ?>
+  </p>
+  <hr>
+  <p>
+    Repeat: <br>
+    <?php echo str_repeat($combine, 2); ?>
+  </p>
+  <p>
+    Make substring from one point to another: <br>
+    <?php echo substr($combine, 0, 10); ?>
+  </p>
+  <!-- string from 0,1,2,3... -->
+  <p>
+    Find Position of any specific word: <br>
+    <?php echo strpos($combine, 'i'); ?> 
+  </p> <!-- finds the position of the first occurrence of a string inside another string. -->
+  <p>
+    Find Character: <br>
+    <?php echo strchr($combine, 'f'); ?>
+  </p>
+    <!-- //find the first character with that condition, prints the string after that character.  -->
+
+
+  <ul>
+    <li>
+      
     </li>
   </ul>
 </body>
