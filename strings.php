72c62bb3f9edeb121407c5e3942e70a4caefebbd
Leily <leilymrz@gmail.com>
2022-05-26 12:44:09 -0700

    strings

---
+++ strings.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Strings</title>
+</head>
+<body>
+  <h1>The God of High School</h1>
+  <?php 
+    echo 'Jin Mori (진모리, Jin Mori; "Mori Jin") is the main protagonist of The God of High School. He is introduced as an arrogant, carefree child obsessed with fighting. <br>';
+    echo 'However his true identity is that of the great God Sun Wukong (The Monkey King), the leader of the Mount Hwagwa Monkey and one of the Nine Kings of the Sage Realm.';
+    $age = 17;
+    $raceSpecies = ['God', 'Demon', 'Mount Hwagwa Monkey'];
+    $statement1 = 'He cannot be defeated by human beings as he is not human himself.';
+    $statement2 = 'To truly defeat him, another Demon or God would have to do that.';
+    $combination = $statement1.' '.$statement2;
+    echo $combination 
+  ?>
+  <ul>
+    <li>
+      <?php echo $age?>
+    </li>
+    <li>
+      <?php 
+        foreach($raceSpecies as $value) {
+          echo $value, ',', ' ';
+        } 
+      ?>
+    </li>
+  </ul>
+</body>
+</html>
